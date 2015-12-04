50b2cf59516524e59956adfbfb890a9dcf01a312
Towa0003 <paulus131313@yahoo.de>
2015-12-04 15:05:52 +0100

    Fixed Login, you can now login with real user data

--- uni.saarland.se.cdit/WebContent/javascript/login.js
+++ uni.saarland.se.cdit/WebContent/javascript/login.js
@@ -1,39 +1,23 @@
 $(document).ready(function () {
-    console.log("Jquery loaded");
-
-
     $("#login").on("click", function () {
-        console.log($("#inputPassword").val());
-        console.log($("#inputEmail").val());
         login($("#inputEmail").val(), $("#inputPassword").val());
     });
-
-
-
-    //login();
-
 });
 
 // Ausgewählte Sprache aus dem Login Screen dem Server melden
 function login(email, pwd) {
+    console.log('login');
+    console.log(email);
+    console.log(pwd);
     $.ajax({
             type: "POST",
             url: "http://localhost:8080/uni.saarland.se.cdit/rest/users/login",
             data: {
-                //                inputEmail: "email",
-                //                inputPassword: "pwd"
-                inputEmail: "test@test.com",
-                inputPassword: "admin"
+                inputEmail: email,
+                inputPassword: pwd
             },
-            //       dataType: 'jsonp',
-            //       jsonp: false,
-            //       jsonpCallback: 'JSONPCallback',
             async: true,
-            //       crossDomain: true,
-            //       username: 'user1',
-            //       password: 'user1',
             success: function (result, textStatus, xhr) {
-                console.log("SUCCESS");
                 console.log(result);
                 console.log(textStatus);
                 console.log(xhr);
@@ -53,30 +37,3 @@ function login(email, pwd) {
 
     function JSONPCallback() {}
 }
-
-//// Ausgewählte Sprache aus dem Login Screen dem Server melden
-//function get() {
-//    $.ajax({
-//            type: "GET",
-//            url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/searchById/1",
-//            //       dataType: 'jsonp',
-//            //       jsonp: false,
-//            //       jsonpCallback: 'JSONPCallback',
-//            async: true,
-//            //       crossDomain: true,
-//            //       username: 'user1',
-//            //       password: 'user1',
-//            success: function (result) {
-//
-//            },
-//            error: function (a, b, c) {
-//                console.log(a + " " + b + " " + c + "ERROR");
-//                document.body.innerHTML = a + " " + b + " " + c + "ERROR";
-//            }
-//        })
-//        .fail(function (e) {
-//            console.log(e.msg + "ERROR");
-//        });
-//
-//    function JSONPCallback() {}
-//}
