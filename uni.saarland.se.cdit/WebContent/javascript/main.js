694a1def0faf09664847adc5877d7f468f3f3507
Towa0003 <paulus131313@yahoo.de>
2015-12-08 10:00:22 +0100

    Added a testing page, with buttons for newTicket GetTicket and deleteTicket

--- uni.saarland.se.cdit/WebContent/javascript/main.js
+++ uni.saarland.se.cdit/WebContent/javascript/main.js
@@ -1,41 +1,40 @@
 $(document).ready(function () {
-    getTicket(1);
+	
+    $("#btnGetTicket1").click(function () {
+    	getTicket(1);
+    });
+    
+    $("#btnGetTicket2").click(function () {
+    	getTicket(2);
+    });
+    
+    $("#btnGetTicket3").click(function () {
+    	getTicket(3);
+    });
+    
+    $("#btnDeleteTicket1").click(function () {
+    	deleteTicket(1);
+    });
+    
+    $("#btnDeleteTicket2").click(function () {
+    	deleteTicket(2);
+    });
+    
+    $("#btnDeleteTicket3").click(function () {
+    	deleteTicket(3);
+    });
+	
+    $("#btnNewTicket").click(function () {
+        newTicket();
+    });
 });
 
-
-
-function getTicket(id){
+function deleteTicket(id){
     $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/searchById/" + id,
-        dataType: 'xml',
-        jsonp: false,
-        //       jsonpCallback: 'JSONPCallback',
-        async: true,
-        //       crossDomain: true,
-        //       username: 'user1',
-        //       password: 'user1',
+        type: "DELETE",
+        url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/remove/" + id,
         success: function (result) {    
-        	
-        	var ticket1 = $('.ticket1');
-        	
-        	var t1_title = $('.ticket1').find('.Title');
-        	t1_title.text(($(result).find('title')).text());
-
-        	
-            //                console.log("SUCCESS1");
-            //                console.log(result);
-            //                var data1 = $(result).find('tickets');
-            //                console.log(data1);
-            //                console.log(data1.text());
-            //                var data2 = $(result).find('ticket');
-            //                console.log(data2);
-            //
-            //                console.log(data2.text());
-            //                var data3 = $(data2).find('title');
-            //                console.log(data3);
-            //
-            //                console.log(data3.text());
+        	console.log("Successfully deleted Ticket" + id);
         },
         error: function (a, b, c) {
             console.log(a + " " + b + " " + c + "ERROR");
@@ -44,81 +43,66 @@ function getTicket(id){
     })
 }
 
-function getTickets() {
-        $.ajax({
-                type: "GET",
-                url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/",
-                dataType: 'xml',
-                jsonp: false,
-                //       jsonpCallback: 'JSONPCallback',
-                async: true,
-                //       crossDomain: true,
-                //       username: 'user1',
-                //       password: 'user1',
-                success: function (result) {    
-                    //                console.log("SUCCESS1");
-                    //                console.log(result);
-                    //                var data1 = $(result).find('tickets');
-                    //                console.log(data1);
-                    //                console.log(data1.text());
-                    //                var data2 = $(result).find('ticket');
-                    //                console.log(data2);
-                    //
-                    //                console.log(data2.text());
-                    //                var data3 = $(data2).find('title');
-                    //                console.log(data3);
-                    //
-                    //                console.log(data3.text());
-                },
-                error: function (a, b, c) {
-                    console.log(a + " " + b + " " + c + "ERROR");
-                    document.body.innerHTML = a + " " + b + " " + c + "ERROR";
-                }
-            })
+function newTicket(){
+	var data = 
+			{
+				"title":"Testticket",
+				"creationDate":"2015-12-05 10:44:52",
+				"description":"test",
+				"priorityId":2,
+				"typeId":1,
+				"statusId":1,
+				"projectId":1
+				}
+	
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: "json",
+        async: true,
+        success: function (result) {    
+        	console.log("SUCCESS!");
+        	console.log(result);
 
-    //    $.ajax({
-    //            type: "GET",
-    //            url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/",
-    //            dataType: 'json',
-    //            jsonp: true,
-    //            //       jsonpCallback: 'JSONPCallback',
-    //            async: true,
-    //            crossDomain: true,
-    //            success: function (result) {
-    //                console.log("SUCCESSJSON");
-    //                console.log(result);
-    //
-    //            },
-    //            error: function (a, b, c) {
-    //                console.log(a + " " + b + " " + c + "ERROR");
-    //                document.body.innerHTML = a + " " + b + " " + c + "ERROR";
-    //            }
-    //        })
-    //        .fail(function (e) {
-    //            console.log(e.msg + "ERROR");
-    //        })
+        },
+        error: function (a, b, c) {
+            console.log(a + " " + b + " " + c + "ERROR");
+            document.body.innerHTML = a + " " + b + " " + c + "ERROR";
+        }
+    })
+}
 
-
-    // Assign handlers immediately after making the request,
-    // and remember the jqxhr object for this request
-//    var jqxhr = $.getJSON("http://localhost:8080/uni.saarland.se.cdit/rest/tickets/", function () {
-//            console.log("success");
-//        })
-//        .done(function () {
-//            console.log("second success");
-//        })
-//        .fail(function (w) {
-//            console.log("error");
-//            console.log(w);
-//        })
-//        .always(function () {
-//            console.log("complete");
-//        });
-
-    // Perform other work here ...
-
-    // Set another completion function for the request above
-    jqxhr.complete(function () {
-        console.log("second complete");
-    });
+function getTicket(id){
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/searchById/" + id,
+        dataType: 'xml',
+        jsonp: false,
+        async: true,
+        success: function (result) {    
+        	var ticket1 = $('.ticket1');
+        	
+        	var t1_title = $('.ticket1').find('.Title');
+        	t1_title.text(($(result).find('title')).text());
+        	
+        	var t1_title = $('.ticket1').find('.Description');
+        	t1_title.text(($(result).find('description')).text());
+        	
+        	var t1_title = $('.ticket1').find('.Priority');
+        	t1_title.text(($(result).find('priorityId')).text());
+        	
+        	var t1_title = $('.ticket1').find('.Type');
+        	t1_title.text(($(result).find('typeId')).text());
+        	
+        	var t1_title = $('.ticket1').find('.Status');
+        	t1_title.text(($(result).find('statusId')).text());
+        },
+        error: function (a, b, c) {
+            console.log(a + " " + b + " " + c + "ERROR");
+            document.body.innerHTML = a + " " + b + " " + c + "ERROR";
+        }
+    })
 }
