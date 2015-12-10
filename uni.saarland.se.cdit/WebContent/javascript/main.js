620587f821d5f6c0497c84d2b923b420be86e53c
Tobias Walter <s8towalt@stud.uni-saarland.de>
2015-12-10 14:49:46 +0100

    Customized testpage

--- uni.saarland.se.cdit/WebContent/javascript/main.js
+++ uni.saarland.se.cdit/WebContent/javascript/main.js
@@ -1,40 +1,48 @@
 $(document).ready(function () {
-	
+
+    $("#btnGetTicket").click(function () {
+        getTicket($("#input_get_ticket").val());
+    });
+
     $("#btnGetTicket1").click(function () {
-    	getTicket(1);
+        getTicket(1);
     });
-    
+
     $("#btnGetTicket2").click(function () {
-    	getTicket(2);
+        getTicket(2);
     });
-    
+
     $("#btnGetTicket3").click(function () {
-    	getTicket(3);
+        getTicket(3);
     });
-    
+
+    $("#btnDeleteTicket").click(function () {
+        deleteTicket($("#input_rmv_ticket").val());
+    });
+
     $("#btnDeleteTicket1").click(function () {
-    	deleteTicket(1);
+        deleteTicket(1);
     });
-    
+
     $("#btnDeleteTicket2").click(function () {
-    	deleteTicket(2);
+        deleteTicket(2);
     });
-    
+
     $("#btnDeleteTicket3").click(function () {
-    	deleteTicket(3);
+        deleteTicket(3);
     });
-	
+
     $("#btnNewTicket").click(function () {
         newTicket();
     });
 });
 
-function deleteTicket(id){
+function deleteTicket(id) {
     $.ajax({
         type: "DELETE",
         url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/remove/" + id,
-        success: function (result) {    
-        	console.log("Successfully deleted Ticket" + id);
+        success: function (result) {
+            console.log("Successfully deleted Ticket" + id);
         },
         error: function (a, b, c) {
             console.log(a + " " + b + " " + c + "ERROR");
@@ -43,18 +51,16 @@ function deleteTicket(id){
     })
 }
 
-function newTicket(){
-	var data = 
-			{
-				"title":"Testticket",
-				"creationDate":"2015-12-05 10:44:52",
-				"description":"test",
-				"priorityId":2,
-				"typeId":1,
-				"statusId":1,
-				"projectId":1
-				}
-	
+function newTicket() {
+    var data = {
+        "title": $("#input_new_title").val(),
+        "description": $("#input_new_desc").val(),
+        "priorityId": $("#input_new_priority").val(),
+        "typeId": $("#input_new_type").val(),
+        "statusId": $("#input_new_status").val(),
+        "projectId": 1
+    }
+
     $.ajax({
         type: "POST",
         url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/",
@@ -63,9 +69,9 @@ function newTicket(){
         crossDomain: true,
         dataType: "json",
         async: true,
-        success: function (result) {    
-        	console.log("SUCCESS!");
-        	console.log(result);
+        success: function (result) {
+            console.log("SUCCESS!");
+            console.log(result);
 
         },
         error: function (a, b, c) {
@@ -75,30 +81,30 @@ function newTicket(){
     })
 }
 
-function getTicket(id){
+function getTicket(id) {
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/uni.saarland.se.cdit/rest/tickets/searchById/" + id,
         dataType: 'xml',
         jsonp: false,
         async: true,
-        success: function (result) {    
-        	var ticket1 = $('.ticket1');
-        	
-        	var t1_title = $('.ticket1').find('.Title');
-        	t1_title.text(($(result).find('title')).text());
-        	
-        	var t1_title = $('.ticket1').find('.Description');
-        	t1_title.text(($(result).find('description')).text());
-        	
-        	var t1_title = $('.ticket1').find('.Priority');
-        	t1_title.text(($(result).find('priorityId')).text());
-        	
-        	var t1_title = $('.ticket1').find('.Type');
-        	t1_title.text(($(result).find('typeId')).text());
-        	
-        	var t1_title = $('.ticket1').find('.Status');
-        	t1_title.text(($(result).find('statusId')).text());
+        success: function (result) {
+            var ticket1 = $('.ticket1');
+
+            var t1_title = $('.ticket1').find('.Title');
+            t1_title.text(($(result).find('title')).text());
+
+            var t1_title = $('.ticket1').find('.Description');
+            t1_title.text(($(result).find('description')).text());
+
+            var t1_title = $('.ticket1').find('.Priority');
+            t1_title.text(($(result).find('priorityId')).text());
+
+            var t1_title = $('.ticket1').find('.Type');
+            t1_title.text(($(result).find('typeId')).text());
+
+            var t1_title = $('.ticket1').find('.Status');
+            t1_title.text(($(result).find('statusId')).text());
         },
         error: function (a, b, c) {
             console.log(a + " " + b + " " + c + "ERROR");
