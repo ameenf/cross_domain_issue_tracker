6c2cf967d5612d27ca824bf12fb8f56aece4fd78
s8chmurl@stud.uni-saarland.de <s8chmurl@stud.uni-saarland.de>
2016-01-02 12:00:11 +0100

    get info of source and target node, label on arrows, endpoints will switch position regarding to position of the node

--- Frontend/js/workflow.js
+++ Frontend/js/workflow.js
@@ -2,32 +2,87 @@ jsPlumb.ready(function () {
     console.log("jsplumb loaded!");
     jsPlumb.setContainer("diagramContainer");
 
-    // variable to describe the look of nodes
+    // Default setting of connections
     var common = {
             connector: ["StateMachine"], // Form of the line between connected nodes
             endpoint: "Dot", // Form of the anchorpoint
+            anchor: ["TopCenter", "RightMiddle", "BottomCenter", "LeftMiddle"],
+            overlays: [["Arrow", {
+                    width: 20,
+                    length: 20,
+                    location: 1,
+                    foldback: 1,
+                    id: "arrow"
+            }],
+                       ["Label", {
+                    labelStyle: {
+                        fillStyle: "white",
+                        font: "15px sans-serif",
+                        color: "rgba(97, 170, 224, 0.72)",
+                    },
+                    label: "Hallo",
+                    location: 0.5,
+                }]
+                      ],
+            paintStyle: {
+                strokeStyle: "gray",
+                lineWidth: 5
+            },
+            endpointStyle: {
+                fillStyle: "rgba(211, 211, 211, 0.7)"
+            },
             isSource: true,
-            isTarget: true
+            isTarget: true,
+            ConnectionsDetachable: true,
+            ReattachConnections: true
         }
         // Anchors can switch position
     var dynamicAnchors1 = ["Left", "Right", "Top", "Bottom"];
     var dynamicAnchors2 = ["Right", "Left", "Top", "Bottom"];
 
+    // Connect nodes
+    jsPlumb.connect({
+        source: "item_1",
+        target: "item_2",
+    }, common);
 
+    jsPlumb.connect({
+        source: "item_2",
+        target: "item_3",
+    }, common);
+    jsPlumb.connect({
+        source: "item_3",
+        target: "item_4",
+    }, common);
+    jsPlumb.connect({
+        source: "item_4",
+        target: "item_5",
+    }, common);
+    jsPlumb.connect({
+        source: "item_5",
+        target: "item_6",
+    }, common);
     // adding anchorpoints to nodes
-    jsPlumb.addEndpoint($(".item"), {
-        anchors: dynamicAnchors1
-    }, common);
+    //    jsPlumb.addEndpoint($(".item"), {
+    //        anchors: dynamicAnchors1
+    //    }, common);
+    //
+    //    jsPlumb.addEndpoint($(".item"), {
+    //        anchors: dynamicAnchors2
+    //    }, common);
 
-    jsPlumb.addEndpoint($(".item"), {
-        anchors: dynamicAnchors2
-    }, common);
-
-    // Node is draggable
+    // Node is draggable on a grid
     jsPlumb.draggable($(".item"), {
         containment: "parent",
         grid: [10, 10]
     });
+
+    // Event to get info about sourceId and targetId of node
+    jsPlumb.bind("connection", function (info) {
+        console.log(info.sourceId);
+        console.log(info.targetId);
+
+    })
 
     //Zoom the container on mousewheel
     $('#diagramContainer').bind('mousewheel', function () {
@@ -47,6 +102,8 @@ jsPlumb.ready(function () {
 
     });
 
+    // ****** ZOOM ******
+    // Set the zoom and scale the overview
     function setZoom(scale, el) {
         console.log(scale)
             //Round scales to nearest 10th
