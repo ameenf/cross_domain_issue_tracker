59f25e27bfbece5532c85ab15843d0525852dd16
Tobias Walter <s8towalt@stud.uni-saarland.de>
2016-01-26 10:06:57 +0100

    Fixed loop error in index

--- uni.saarland.se.cdit/WebContent/js/main.js
+++ uni.saarland.se.cdit/WebContent/js/main.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
     //Check for authentication
     if ((Cookies.get('loggedin') == "true") || (window.location = "index.html")) {
-        console.log("jow");
+
     } else {
         console.log("window.location");
         window.stop();
         window.location = "index.html";
     }
 });
-
-function changePage(newurl) {
-    location.href = baseurl + newurl;
-}
