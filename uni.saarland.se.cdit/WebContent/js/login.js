75fb4f3cf27f131ae364b874a4774f61610e322a
s8towalt <s8towalt@stud.uni-saarland.de>
2016-02-06 11:48:05 +0100

    Reworked the login screen

--- uni.saarland.se.cdit/WebContent/js/login.js
+++ uni.saarland.se.cdit/WebContent/js/login.js
@@ -12,6 +12,12 @@ function callbackSuccessLogin(result) {
     console.log(result);
     Cookies.set('loggedin', 'true');
     Cookies.set('userid', result.id);
+
+    $("#login-username").removeClass('has-error');
+    $("#login-password").removeClass('has-error');
+    $("#login-username").addClass('has-success');
+    $("#login-password").addClass('has-success');
+
     changePage("projects.html")
 
     console.log("Logging in");
@@ -22,6 +28,15 @@ function callbackFailedLogin(a, b, c) {
     console.log(a);
 
     console.log("Login failed");
+    //    $("#loginFailBox").show();
+
+    $("#login-username").addClass('has-error');
+    $("#login-password").addClass('has-error');
+
     $("#loginFailBox").show();
+    $("#loginFailBox").fadeTo(2000, 500).slideUp(1000, function () {
+        //                                $("#connectionExistsAlert").alert('close');
+        $("#loginFailBox").hide();
+    });
     //$("#loginFailBox").css('color', 'red');
 }
