5852efa79cd308d241f2afd100d9ef171a79e4a7
Alex Grethen <alexgrethen@hotmail.com>
2016-02-11 20:57:44 +0100

    group permissions untested

--- uni.saarland.se.cdit/WebContent/js/rest.js
+++ uni.saarland.se.cdit/WebContent/js/rest.js
@@ -1,4 +1,4 @@
-var port = 8080;
+var port = 8990;
 var baseurl = 'http://localhost:' + port + '/uni.saarland.se.cdit/';
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////Projects/////////////////////////////////////////////////////
@@ -1357,6 +1357,31 @@ function getGroups() {
     
 }
 
+function getGroupsWithPermissions(id) {
+    $.ajax({
+        type: "GET",
+        //url: "http://localhost:8990/uni.saarland.se.cdit/rest/general/priority",
+        url: baseurl + "rest/users/groups",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Basic ' + btoa(Cookies.get('username') + ':' + Cookies.get('password')));
+        },
+        dataType: 'json',
+        async: true,
+        success: function (result) {
+            //            return result;
+            callbackGetGroupsWithPermissions(result, id);
+        },
+        error: function (a, b, c) {
+            console.log(a + " " + b + " " + c + "ERROR");
+            document.body.innerHTML = a + " " + b + " " + c + "ERROR";
+            if (c == "Unauthorized") {
+                window.location.href = baseurl;
+            }
+        }
+    })
+    
+}
+
 function createGroup(name) {
     var data = {
         "description": "",
@@ -1381,6 +1406,69 @@ function createGroup(name) {
         success: function (result) {
             //            return result;
             callbackCreateGroup(result);
+        },
+        error: function (a, b, c) {
+            console.log(a + " " + b + " " + c + "ERROR");
+            document.body.innerHTML = a + " " + b + " " + c + "ERROR";
+            if (c == "Unauthorized") {
+                window.location.href = baseurl;
+            }
+        }
+    })
+    
+}
+
+function deleteGroup(id) {
+    $.ajax({
+        type: "DELETE",
+        //url: "http://localhost:8990/uni.saarland.se.cdit/rest/general/priority",
+        url: baseurl + "rest/users/groups/remove/" + id,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Basic ' + btoa(Cookies.get('username') + ':' + Cookies.get('password')));
+        },
+        dataType: 'json',
+        async: true,
+        success: function (result) {
+            //            return result;
+            console.log(result);
+            location.reload();
+        },
+        error: function (a, b, c) {
+            console.log(a + " " + b + " " + c + "ERROR");
+            document.body.innerHTML = a + " " + b + " " + c + "ERROR";
+            if (c == "Unauthorized") {
+                window.location.href = baseurl;
+            }
+        }
+    })
+    
+}
+
+function updateGroup(id, name, description, permissions) {
+    var data = {
+        "id": id,
+        "description": description,
+        "name": name,
+        "permissions": permissions,
+    }
+    
+    console.log(data);
+    
+    $.ajax({
+        type: "PUT",
+        //url: "http://localhost:8990/uni.saarland.se.cdit/rest/general/priority",
+        url: baseurl + "rest/users/groups",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Basic ' + btoa(Cookies.get('username') + ':' + Cookies.get('password')));
+        },
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: "json",
+        async: true,
+        success: function (result) {
+            //            return result;
+            location.reload();
         },
         error: function (a, b, c) {
             console.log(a + " " + b + " " + c + "ERROR");
