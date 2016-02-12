2087dd4a1d965545b521e7b2e028415a201c8655
s8towalt <s8towalt@stud.uni-saarland.de>
2016-02-12 16:58:26 +0100

    Filtering nodes

--- uni.saarland.se.cdit/WebContent/js/workflowFilter.js
+++ uni.saarland.se.cdit/WebContent/js/workflowFilter.js
@@ -1,3 +1,4 @@
+'use strict';
 $(document).ready(function () {
     $('.filterWorkflow').on('keyup', function () {
         filterWorkflow();
@@ -8,7 +9,26 @@ function filterWorkflow() {
     //$('.item').removeClass("highlighted")
     console.log("workflowfilter");
     var searchkey = $('.filterWorkflow').val();
-    console.log('#' + searchkey + 'node');
-    console.log($('#' + searchkey + 'node'));
-    $('#' + searchkey + 'node').addClass("highlighted")
+    var that = this;
+    $('.topTitle').each(function (i, obj) {
+        //        console.log(i);
+        //        console.log(obj);
+        //        console.log(obj.innerHTML);
+        console.log($(obj).parent());
+        console.log(obj.innerHTML.toLowerCase());
+        console.log('===');
+        console.log(searchkey.toLowerCase());
+        if (searchkey.length <= 0) {
+            $(obj).parent().removeClass("highlighted");
+        } else if (obj.innerHTML.toLowerCase().indexOf(searchkey.toLowerCase()) > -1) {
+            $(obj).parent().addClass("highlighted");
+        } else {
+            $(obj).parent().removeClass("highlighted");
+        }
+    });
+
+    //    console.log($('.topTitle'));
+    //    console.log($('.topTitle').first());
+    //    console.log($('.topTitle').val());
+    //$('#' + searchkey + 'node').addClass("highlighted")
 }
