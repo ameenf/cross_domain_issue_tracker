3e4811109c667b3a5afb60014114a669107ca1a8
s8towalt <s8towalt@stud.uni-saarland.de>
2016-02-12 22:55:48 +0100

    More File improvements

--- uni.saarland.se.cdit/WebContent/js/files.js
+++ uni.saarland.se.cdit/WebContent/js/files.js
@@ -1,10 +1,11 @@
 "use strict";
 var allFiles = [];
+var allTickets = [];
 
 $(document).ready(function () {
     console.log("issues.js");
 
-
+    getTicketsByProject(Cookies.get('projectid'));
     //Changehandler for the search bar. Change something and press enter to call this
     $('.filterFiles').on('keyup', function () {
         filterFiles();
@@ -16,8 +17,6 @@ $(document).ready(function () {
         var str = $(this).attr('class');
         window.open(baseurl + "rest/files/" + split_id[1] + "/" + $(this).html());
     });
-
-    getFiles(Cookies.get('projectid'));
 });
 
 function callbackGetFiles(result) {
@@ -44,8 +43,26 @@ function filterFiles() {
 
 function addFileRow(id, title, ticketid) {
     console.log("addFileRow");
+
+    var ticket = $.grep(allTickets, function (e) {
+        return e.id == id;
+    });
+
+    console.log("ticket", ticket);
+
     $('.files').append('<li class="itemRow file' + id + '"></li>');
-    $('.file' + id).append('<div class="flexrow centeritems flexspacebetween innerFile' + id + '"></div>');
+    $('.file' + id).append('<div class="flexrow centeritems flexspacebetween innerFile innerFile' + id + '"></div>');
     $('.innerFile' + id).append('<a class="itemName">' + title + '</a>');
-    $('.project' + id).append('<div class="dividerHorizontal"></div>');
+    if (ticket.length > 0) {
+        $('.innerFile' + id).append('<div class="itemInfo">' + ticket[0].title + '</div>');
+        $('.innerFile' + id).append('<div class="itemInfo">' + ticket[0].creationDate + '</div>');
+    }
+
+    $('.file' + id).append('<div class="dividerHorizontal"></div>');
+}
+
+function callbackGetTicketsByProject(result) {
+    console.log(result);
+    allTickets = result;
+    getFiles(Cookies.get('projectid'));
 }
